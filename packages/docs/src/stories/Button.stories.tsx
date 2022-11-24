a63d8f758f557c22f43642d156ef74eadc28e3be
Thiago <gmoliveira69@gmail.com>
2022-11-23 22:18:53 -0300

    box component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -4,6 +4,9 @@ import { Button, ButtonProps } from '@ignite-ui/react'
 export default {
     title: "Button",
     component: Button,
+    args: {
+        children: "Enviar"
+    }
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {
