fde360084e07e1b75c7b4906458a92da879ed53a
Thiago <gmoliveira69@gmail.com>
2022-12-04 17:44:28 -0300

    Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/77520434?s=96&v=4',
+    alt: 'Thiago Batista',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
--- packages/react/src/components/Avatar/index.tsx
+++ packages/react/src/components/Avatar/index.tsx
@@ -1,7 +1,17 @@
-import { AvatarContainer } from "./styles";
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarImage, AvatarFallback } from './styles'
 
-export function Avatar () {
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
   return (
-    <AvatarContainer />
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
   )
 }
