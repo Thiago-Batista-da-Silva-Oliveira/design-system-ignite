0c6a7cf0383f603b999736f6bf9705dcb7cf8c67
Thiago <gmoliveira69@gmail.com>
2022-12-04 17:07:04 -0300

    avatar

---
+++ packages/react/src/components/Avatar/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from "../../styles";
+import * as Avatar from "@radix-ui/react-avatar"
+
+export const AvatarContainer = styled(Avatar.Root, {
+
+})
+
+export const AvatarImage = styled(Avatar.Image, {
+
+})
